e668b13a73abb2d52bf1af4b9996a93949027daf
Tommy Tran <haitran1995@gmail.com>
2022-10-06 08:02:47 -0400

    Remove default react apps

--- src/App.js
+++ src/App.js
@@ -4,24 +4,9 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
     </div>
   );
 }
 
 export default App;
 
-https://web.compass.lighthouselabs.ca/976a5820-e665-47d7-b057-05de187e766b
